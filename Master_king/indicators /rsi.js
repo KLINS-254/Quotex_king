9c3ac08743878ce9a6c082d1f9b737d57dc97f6e
KLINS-254 <klinswoo@gmail.com>
2025-09-27 14:30:18 +0300

    Delete Master_king/indicators /rsi.js

--- Master_king/indicators /rsi.js
+++
@@ -1,9 +0,0 @@
-// indicators/rsi.js
-import { RSI } from 'technicalindicators';
-
-export function calculateRSI(closePrices, period = 14) {
-  if (closePrices.length < period) return null;
-  const input = { values: closePrices, period: period };
-  const rsiValues = RSI.calculate(input);
-  return rsiValues[rsiValues.length - 1];
-}
